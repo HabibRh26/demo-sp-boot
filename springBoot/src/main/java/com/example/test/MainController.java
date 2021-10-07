03deb20b8f9a2ee3a6225eacfb375c32884f60e1
HabibRh26 <hremon62@gmail.com>
2021-10-07 16:26:02 +0600

    spring boot mvc done

--- springBoot/src/main/java/com/example/test/MainController.java
+++ springBoot/src/main/java/com/example/test/MainController.java
@@ -3,10 +3,8 @@ package com.example.test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import repo.CoderRepo;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +15,6 @@ public class MainController {
     @Autowired
     CoderRepo repoObj;
 
-    @ModelAttribute
-    public void welcomMsg(Model m){
-        m.addAttribute("msg","Welcome Message for All");
-    }
-
 //    @RequestMapping("/home")
     @GetMapping("/home")
     public String homepage(){
@@ -31,22 +24,46 @@ public class MainController {
 //    @RequestMapping(value="/addCoder", method = RequestMethod.POST)
     @PostMapping("/addCoder")
     public String addProg(@ModelAttribute("c") Coder coder){
+        repoObj.save(coder);
+        return "redirect:/home";
+    }
+    @PostMapping("/findById")
+    public String findById(@RequestParam int pId, Model model){
+       Coder coderObj = repoObj.getById(pId);
+       model.addAttribute("c", coderObj);
 
+       return "coder.html";
+    }
+
+    @GetMapping("/delById")
+    public String deleteById(@RequestParam int pId){
+        repoObj.deleteById(pId);
+        return "redirect:/home";
+    }
+
+    @PostMapping("/update")
+    public String Update(@ModelAttribute("c") Coder coder){
+
+        Coder cd = repoObj.getById( coder.getpId());
+        cd.setpName(coder.getpName());
+        cd.setpLang(coder.getpLang());
+        repoObj.save(cd);
         return "coder.html";
     }
-    @GetMapping("/allCoder")
-    public String allCoder(Model model){
-        List<Coder> cd = new ArrayList<Coder>();
-        cd.add(new Coder(45,"hank","c"));
-        cd.add(new Coder(56,"white","cpp"));
-        cd.add(new Coder(89,"pinkman","c#"));
 
-        model.addAttribute("coders",cd);
+    @PostMapping("/findByName")
+    public String findByName(@RequestParam String pName, Model model){
+        List<Coder> cd = repoObj.findBypName(pName);
+        model.addAttribute("coders", cd);
         return "allCoder.html";
     }
 
-
-
+    @PostMapping("/findByLang")
+    public String findByLang(@RequestParam String pLang, Model model){
+        List<Coder> cd = repoObj.findByLang(pLang);
+        model.addAttribute("coders", cd);
+        return "allCoder.html";
+    }
 
 
 
--- springBoot/src/main/java/com/example/test/TestApplication.java
+++ springBoot/src/main/java/com/example/test/TestApplication.java
@@ -1,9 +1,13 @@
 package com.example.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//@EnableAutoConfiguration
+//@SpringBootApplication(scanBasePackages={"com.example.test","repo"})
 public class TestApplication {
 
     public static void main(String[] args) {
