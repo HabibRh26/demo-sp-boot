97debab8ab581616d5416fa6356207733d1fdf82
HabibRh26 <hremon62@gmail.com>
2021-11-03 15:21:22 +0600

    my pc changes

--- basic-rest-api-crud/src/main/java/com/example/restapicrud/service/EmployeeService.java
+++ basic-rest-api-crud/src/main/java/com/example/restapicrud/service/EmployeeService.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
+    List<Employee> saveEmployees(List<Employee> employees);
     List<Employee> getAllEmployees();
     Employee getEmployeeById(long id);
     Employee updateEmployee(Employee employee, long id);
+    Employee updateEmployeeAlt(Employee employee);
     void deleteEmployee(long id);
 }
